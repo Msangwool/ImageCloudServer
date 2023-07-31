98d3c8bb69838d136242516e9d4f5688d5b515f5
sangwoo <tkddnwkdb@gmail.com>
2023-07-31 15:39:54 +0900

    feat: Add Init Set

---
+++ src/main/java/org/awstest/imagecloudserver/ImageCloudServerApplication.java
@@ -0,0 +1,13 @@
+package org.awstest.imagecloudserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageCloudServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageCloudServerApplication.class, args);
+    }
+
+}
