0a2f576b5e79ab902e9c350e01ef23f54c11c3bb
sangwoo <tkddnwkdb@gmail.com>
2023-08-08 11:55:10 +0900

    feat(Controller): Aws S3 접근 가능 API 수정 및 에러 처리 추가

--- src/main/java/org/awstest/imagecloudserver/controller/AwsController.java
+++ src/main/java/org/awstest/imagecloudserver/controller/AwsController.java
@@ -1,40 +1,55 @@
 package org.awstest.imagecloudserver.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.awstest.imagecloudserver.dto.FileDeleteDto;
-import org.awstest.imagecloudserver.dto.FileNameDto;
-import org.awstest.imagecloudserver.fileIO.FileStore;
+import org.awstest.imagecloudserver.core.File;
+import org.awstest.imagecloudserver.dto.*;
+import org.awstest.imagecloudserver.service.FileService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/aws")
 public class AwsController {
 
-    private final FileStore fileStore;
+    private final FileService fileService;
 
     @PostMapping("/create")
-    public ResponseEntity<?> uploadFile(MultipartFile multipartFile) throws IOException {
-        return ResponseEntity.ok().body(fileStore.create(multipartFile));
+    public ResponseEntity<?> uploadFile(FileSaveReqDto fileSaveReqDto) {
+        try {
+            fileService.save(fileSaveReqDto);
+            return ResponseEntity.ok().body(null);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(null);
+        }
     }
 
-    @GetMapping("/read/{fileName}")
-    public ResponseEntity<?> findFile(@PathVariable String fileName) throws IOException {
-        return fileStore.read(fileName);
+    @GetMapping("/read/file")
+    public ResponseEntity<?> findFileInfo(FileReadReqDto fileReadReqDto) {
+        Optional<File> fileInfo = fileService.getFileInfo(fileReadReqDto);
+        if (fileInfo.isEmpty()) {
+            return ResponseEntity.badRequest().body(null);
+        }
+        return ResponseEntity.ok().body(fileInfo.get());
+    }
+
+    @GetMapping("/read/resource")
+    public ResponseEntity<?> readFile(FileReadReqDto fileReadReqDto) throws IOException {
+        return fileService.readFile(fileReadReqDto);
     }
 
     @PostMapping("/update")
-    public ResponseEntity<?> updateFile(FileDeleteDto fileDeleteDto) throws IOException {
-        return ResponseEntity.ok().body(fileStore.update(fileDeleteDto));
+    public ResponseEntity<?> updateFile(FileUpdateReqDto fileUpdateReqDto) {
+        fileService.update(fileUpdateReqDto);
+        return ResponseEntity.ok().body(null);
     }
 
     @PostMapping("/delete")
-    public ResponseEntity<?> deleteFile(@RequestBody FileNameDto fileName) {
-        fileStore.delete(fileName.getFileName());
+    public ResponseEntity<?> deleteFile(@RequestBody FileDeleteReqDto fileDeleteReqDto) {
+        fileService.delete(fileDeleteReqDto);
         return ResponseEntity.ok().body(null);
     }
 }
